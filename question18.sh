0eca6242ff06a8276291d9b938f17069a37a1beb
anhdo1896 <anhdongoc18@gmail.com>
2023-06-04 14:39:40 +0930

    final

--- question18.sh
+++ question18.sh
@@ -1,19 +1,22 @@
 git clone https://github.com/ian-knight-uofa/git-practice-04.git
-git branch -r
-git checkout ready1
-git checkout ready2
-git checkout ready3
-git checkout main
+git branch -a
+
+for branch in $(git branch -r | grep 'ready'(;do
+	git branch ${branch#origin/} $branch
+done
+
+
 git merge ready1 
 git merge ready2
 git merge ready3
+
 git branch -d ready1
 git branch -d ready2
 git branch -d ready3
-git checkout update1
-git pull
-git add .
-git commit -m "update1"
-git checkout update2
-git pull git add .
-git commit -m "update2"
+
+git branch update1
+git merge update1
+
+git branch update2
+git merge update2
+
